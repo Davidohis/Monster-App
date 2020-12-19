dc27d7af70ca4361e6fcbc50d6e7438c39877c79
Davidohis <davidi.ohis25@gmail.com>
2020-12-19 10:58:44 +0000

    add redux to the app

--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -1,7 +1,14 @@
+import { CHANGE_SEARCH_FIELD } from './constants'
+
 const intialState = {
     searchField: ''
 }
 
 export const searchRobots = (sate=intialState, action={}) => {
-
+    switch(action.type) {
+       case CHANGE_SEARCH_FIELD:
+         return Object.assign({}, state, {searchField: action.payload});
+       default:
+         return state;
+    }
 }
